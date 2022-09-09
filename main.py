ebfe4f867524ee147134744f8c775990c8646228
甲甲 <51043917+jiajiaxd@users.noreply.github.com>
2022-09-09 18:51:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import os
+from moviepy.editor import *
+import os
+from natsort import natsorted
+
+def editvideo():
+    L = []
+    for root, dirs, files in os.walk("newsplayer"):
+        # 按文件名排序
+        files = natsorted(files)
+        # 遍历所有文件
+        for file in files:
+            # 如果后缀名为 .mp4
+            if os.path.splitext(file)[1] == '.mp4':
+                # 拼接成完整路径
+                filePath = os.path.join(root, file)
+                # 载入视频
+                video = VideoFileClip(filePath)
+                # 添加到数组
+                L.append(video)
+
+    # 拼接视频
+    final_clip = concatenate_videoclips(L)
+
+    # 生成目标视频文件
+    final_clip.to_videofile("newsplayer\\target.mp4",
+                            fps=24, remove_temp=False)
+
+if __name__ == '__main__':
+    os.system("pip config set global.trusted-host mirrors.aliyun.com ")
+    os.system(
+        "pip config set global.index-url http://mirrors.aliyun.com/pypi/simple/")
+    os.system("pip install -r requirements.txt")
+    response = requests.get(
+        "https://api.cntv.cn/lanmu/columnSearch?&fl=&fc=%E6%96%B0%E9%97%BB&cid=&p=1&n=20&serviceId=tvcctv&t=jsonp&cb"
+        "=Callback")
+    res = json.loads(response.text.removeprefix('Callback(').removesuffix(');'))
+    res = res.get('response').get("docs")[14].get('lastVIDE')
+    print("获取到", res.get('videoTitle'), res.get('videoUrl'))
+    os.system("you-get -n -o newsplayer "+res.get('videoUrl'))
+    editvideo()
